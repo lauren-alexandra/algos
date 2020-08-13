1b3e4adc10c0cd434fdf65135f7ece8adedb1b08
Lauren Alexandra <lauren@laurenalexandra.dev>
2020-08-12 23:25:50 -0700

    reverse substrings, backspace compare stacks

--- interview-help/stacks_examples.js
+++ interview-help/stacks_examples.js
@@ -44,3 +44,71 @@ var minAddToMakeValid = function(S) {
     // if 0 then balanced, valid
     return a + balance; 
 };
+
+// #1190 reverse substrings between each pair of parentheses
+// not all tests passed 
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseParentheses = function(s) {
+    var str = s; 
+    var strLen = str.length; 
+    var tmp = "";
+    var finStr = ""; 
+    var strReversed = ""; 
+    var arr = s.split("");
+    var stack = new Array(); 
+    
+    for(var j = 0; j < strLen; j++) {
+        if(str[j] === '\('){
+            stack.push(j);
+        }
+        if(str[j] === '\)'){
+            // reverse string starting from beginning of ( 
+            tmp = str.slice(stack[-1], j + 1).split("").reverse().join("");
+            strReversed += tmp.slice(0, -1);
+            // remove last ( index from stack 
+            stack.splice(-1, 1);
+        }   
+    };
+    
+    // create final string without parentheses
+    for(var k = 0; k < strReversed.length; k++) {
+        if(strReversed[k] !== '\(' && strReversed[k] !== '\)') {
+            finStr += strReversed[k];
+        }
+    } 
+    
+    return finStr;
+};
+
+
+// backspace compare
+
+function backspaceCompare(str1, str2) {
+  var strStack1 = str1.split("");  // S = "ab#c" => ['a', 'b', '#', 'c'] 
+  var strStack2 = str2.split(""); 
+  var finStack1 = new Array(); 
+  var finStack2 = new Array(); 
+  var myStacks = [
+    [strStack1, finStack1], 
+    [strStack2, finStack2] 
+  ]; 
+  
+  myStacks.forEach(function(group) {
+    var initialStack = group[0]; 
+    var modStack = group[1]; 
+    while(initialStack.length > 0) {
+      if(initialStack[-1] === '#') {
+        initialStack.pop();  
+        initialStack.pop();
+      } else {
+        modStack.push(initialStack[-1]); 
+      }
+    }
+  }); 
+  
+  // compare strings 
+  return finStack1.join("") === finStack2.join(""); 
+}

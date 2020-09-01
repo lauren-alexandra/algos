254c4a3751c5965bfe84b0d04f023803934f5e6b
Lauren Alexandra <lauren@laurenalexandra.dev>
2020-08-31 23:56:36 -0700

    max avg

---
+++ interview-help/array_problems.js
@@ -0,0 +1,57 @@
+/* #643 Maximum Average Subarray I
+
+Given an array consisting of n integers, find the contiguous subarray of given length k that has the maximum average value. And you need to output the maximum average value.
+
+Example 1:
+
+Input: [1,12,-5,-6,50,3], k = 4
+Output: 12.75
+Explanation: Maximum average is (12-5-6+50)/4 = 51/4 = 12.75
+*/ 
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function(nums, k) {
+    sum = new Array(nums.length); 
+    sum[0] = nums[0];
+    for(var i = 1; i < nums.length; i++) {
+        sum[i] = sum[i - 1] + nums[i]
+        res = sum[k - 1] * 1.0 / k;
+    }
+    for(var i = k; i < nums.length; i++) {
+        res = Math.max(res, (sum[i] - sum[i - k]) * 1.0 / k); 
+    }
+    
+    return res; 
+};
+
+/* #189 Rotate Array
+
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Follow up:
+
+Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+Could you do it in-place with O(1) extra space?
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+*/ 
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    
+};

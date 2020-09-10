8163989f5f7c9db25581826b1e5326da34109f0e
Lauren Alexandra <lauren@laurenalexandra.dev>
2020-09-09 23:42:29 -0700

    DFS similar sentences

--- interview-help/graphs.js
+++ interview-help/graphs.js
@@ -167,3 +167,81 @@ F
 B
 G
 C
+
+/*
+737. Sentence Similarity II
+
+Given two sentences words1, words2 (each represented as an array of strings), and a list of similar word pairs pairs, determine if two sentences are similar.
+
+For example, words1 = ["great", "acting", "skills"] and words2 = ["fine", "drama", "talent"] are similar, if the similar word pairs are pairs = [["great", "good"], ["fine", "good"], ["acting","drama"], ["skills","talent"]].
+
+Note that the similarity relation is transitive. For example, if "great" and "good" are similar, and "fine" and "good" are similar, then "great" and "fine" are similar.
+
+Similarity is also symmetric. For example, "great" and "fine" being similar is the same as "fine" and "great" being similar.
+
+Also, a word is always similar with itself. For example, the sentences words1 = ["great"], words2 = ["great"], pairs = [] are similar, even though there are no specified similar word pairs.
+
+Finally, sentences can only be similar if they have the same number of words. So a sentence like words1 = ["great"] can never be similar to words2 = ["doubleplus","good"].
+
+Note:
+
+The length of words1 and words2 will not exceed 1000.
+The length of pairs will not exceed 2000.
+The length of each pairs[i] will be 2.
+The length of each words[i] and pairs[i][j] will be in the range [1, 20].
+*/ 
+
+// DFS
+var areSentencesSimilarTwo = function(words1, words2, pairs) {
+    // sentences must have same word count 
+    if(words1.length !== words2.length) {
+        return false; 
+    }
+    
+    var vertices = words1.concat(words2); 
+    var g = new Graph(vertices.length); 
+
+    // adding vertices 
+    for (var i = 0; i < vertices.length; i++) { 
+        g.addVertex(vertices[i]); 
+    } 
+
+    // adding edges 
+    pairs.forEach(function(pair){
+        const word1 = pair[0];
+        const word2 = pair[1]; 
+        
+        g.addEdge(word1, word2); 
+    }); 
+    
+    words1.forEach((word1, index) => {
+      const word2 = words2[index];
+      let stack = [word1];
+      let visited = new Set();
+      visited.add(word1);  
+        
+      if (stack.length > 0) {
+        while(stack.length > 0) {
+          let word = stack.pop();
+            
+          // if word similar with itself
+          if(word == word2) {
+              break; 
+          }  
+          
+          // 1. In the edges object, we search for nodes this node is directly connected to.
+          // 2. We filter out the nodes that have already been visited.
+          // 3. Then we mark each unvisited node as visited and push it to the Stack.
+          g.AdjList[word].filter(node => !visited.has(node)).forEach(node => {
+             visited.add(node);
+             stack.push(node);
+            });
+        }
+      }
+      else {
+          return false; 
+      } 
+    });
+        
+    return true; 
+};

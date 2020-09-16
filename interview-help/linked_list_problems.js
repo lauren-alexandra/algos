09dadc7f037397d814b5fdb7b99029f3fc2e3b4d
Lauren Alexandra <lauren@laurenalexandra.dev>
2020-09-16 06:25:20 -0700

    merge two sorted linked lists

--- interview-help/linked_list_problems.js
+++ interview-help/linked_list_problems.js
@@ -100,5 +100,55 @@ var removeNthFromEnd = function(head, n) {
     return hPointer.next; 
 };
 
+/*
+21. Merge Two Sorted Lists
 
+Merge two sorted linked lists and return it as a new sorted list. The new list should be made by splicing together the nodes of the first two lists.
 
+Example:
+
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+  // if either list is null, return list not null 
+  if (l1 === null) {
+    return l2; 
+  }
+  else if (l2 === null) {
+    return l1;  
+  }
+  // traverse both lists
+  // if head of a list is less than head of other list
+  // set that head as the next value
+  // in the merged list 
+    
+  // * do this through recursion e.g. 
+  // mergeTwoLists(l1, l2.next) 
+
+  // return merged list 
+  
+  else if (l1.next < l2.next) {
+   l1.next = mergeTwoLists(l1.next, l2); 
+
+   return l1; 
+  }
+  else {
+   l2.next = mergeTwoLists(l1, l2.next); 
+
+   return l2;
+  }
+};

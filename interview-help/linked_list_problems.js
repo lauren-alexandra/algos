c7730d8daa90ac201f063676ca48663fe92b3d76
Lauren Alexandra <lauren@laurenalexandra.dev>
2020-09-16 06:22:20 -0700

    remove nth node from end of list

--- interview-help/linked_list_problems.js
+++ interview-help/linked_list_problems.js
@@ -42,6 +42,63 @@ var middleNode = function(head) {
     return slow; 
 };
 
+/*
+19. Remove Nth Node From End of List
+
+Given a linked list, remove the n-th node from the end of list and return its head.
+
+Example:
+
+Given linked list: 1->2->3->4->5, and n = 2.
+
+After removing the second node from the end, the linked list becomes 1->2->3->5.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+    // get the head node in case head removal 
+    let hPointer = new ListNode(0); 
+    hPointer.next = head;
+    let first = head; 
+    let length = 0; 
   
+    // need to find the length of the list 
+    // why: this gives us a location: length - n for nth node
+    while (first != null) {
+        length++;
+        first = first.next;
+    }
+  
+    // then need to traverse the list 
+    /*     1    2
+      1 -> 2 -> 3
+      1 -> 3 */
+    
+    // length - n for nth node 
+    length = length - n; 
+    first = hPointer;
+    while(length > 0) {
+      length--;
+      first = first.next; 
+    } 
+    
+    // and point to (length - n) + 1 to 'skip' over nth node
+    first.next = first.next.next; 
+  
+    // return head of list
+    return hPointer.next; 
+};
+
 
 

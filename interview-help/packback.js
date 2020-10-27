f8f25f2cdb406ed875bfec1ea32ad9dca309f6a5
Lauren Alexandra <lauren@laurenalexandra.dev>
2020-10-27 12:36:50 -0700

    length of longest fib subsequence

--- interview-help/packback.js
+++ interview-help/packback.js
@@ -48,3 +48,55 @@ var fib = function(N) {
     return current; 
 };
 
+/*
+#873 Length of Longest Fibonacci Subsequence
+
+A sequence X_1, X_2, ..., X_n is fibonacci-like if:
+
+n >= 3
+X_i + X_{i+1} = X_{i+2} for all i + 2 <= n
+Given a strictly increasing array A of positive integers forming a sequence, find the length of the longest fibonacci-like subsequence of A.  If one does not exist, return 0.
+
+(Recall that a subsequence is derived from another sequence A by deleting any number of elements (including none) from A, without changing the order of the remaining elements.  For example, [3, 5, 8] is a subsequence of [3, 4, 5, 6, 7, 8].)
+
+ 
+
+Example 1:
+
+Input: [1,2,3,4,5,6,7,8]
+Output: 5
+Explanation:
+The longest subsequence that is fibonacci-like: [1,2,3,5,8].
+Example 2:
+
+Input: [1,3,7,11,12,14,18]
+Output: 3
+Explanation:
+The longest subsequence that is fibonacci-like:
+[1,11,12], [3,11,14] or [7,11,18].
+*/
+
+/**
+ * @param {number[]} A
+ * @return {number}
+ */
+var lenLongestFibSubseq = function(A) {
+    let N = A.length;
+     let index = new Map();
+     A.map((v, i) => index[v] = i);
+ 
+     let longest = new Map();
+     let ans = 0;
+     for (let k = 0; k < N; ++k)
+         for (let j = 0; j < k; ++j) {
+             let i = index[A[k] - A[j]];
+             if (i === undefined || i >= j) continue;
+             if (longest[i * N + j] === undefined)
+                 longest[i * N + j] = 2;
+ 
+             longest[j * N + k] = longest[i * N + j] + 1;
+             ans = Math.max(ans, longest[j * N + k]);
+         }
+ 
+     return ans >= 3 ? ans : 0; 
+ };

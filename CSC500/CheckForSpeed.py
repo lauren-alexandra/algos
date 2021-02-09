e5ae5911f4ce792b639dd44b55e5bf19c2616a96
Lauren Alexandra <lauren@laurenalexandra.dev>
2021-02-09 13:23:51 -0800

    zerodiv

--- CSC500/CheckForSpeed.py
+++ CSC500/CheckForSpeed.py
@@ -6,20 +6,25 @@ import sys
 
 def main():
     try:
-        num1 = float(input('Input first number:'))
-    except ValueError:
-        print("Not a number.")
-        sys.exit()
-    try: 
-        num2 = float(input('Input second number:'))
+        num1 = float(input('Enter a number: '))
     except ValueError:
         print("Not a number.")
         sys.exit()
 
+    try: 
+        num2 = float(input('Enter a non-zero number: '))
+    except ZeroDivisionError:
+        print("Can not divide by 0.")
+        sys.exit()
+    else:
+        print("Not a number.")
+        sys.exit()
+
+    print(num2)
     product = num1 * num2
-    quotient = num1//num2
+    #quotient = num1 / num2
     print("Product: ", product)
-    print("Quotient: ", quotient)
+    #print("Quotient: ", quotient)
 
 if __name__ == "__main__":
     main()

b82c7306bdc7fda296d3194058b72a8ebab52fbf
Lauren Alexandra <lauren@laurenalexandra.dev>
2021-02-09 14:56:13 -0800

    readonly checkforspeed

--- CSC500/Week1/CheckForSpeed.py
+++ CSC500/Week1/CheckForSpeed.py
@@ -1,25 +1,47 @@
 """
-Finds the multiplication and division of two numbers.
+Title: Check for Speed
+Description: Finds the multiplication and division of two numbers.
+
+Term: Winter D
+Course: CSC 500
+Instructor: Amr Elchouemi
+
+Author: Lauren Alexandra
+Email: lauren.alexandra@csuglobal.edu
 """
 
 import sys
+import os
+import stat
 
 def main():
+    f = open('CheckForSpeedResults.txt', 'w+')
+
     try:
         num1 = float(input('Enter a number: '))
     except ValueError:
-        print("Not a number.")
+        print("Not a number.", file = f)
         sys.exit()
+
     try: 
         num2 = float(input('Enter another number: '))
     except ValueError:
-        print("Not a number.")
+        print("Not a number.", file = f)
         sys.exit()
 
     product = num1 * num2
-    quotient = num1 / num2
-    print("Product: ", product)
-    print("Quotient: ", quotient)
+
+    try:
+        quotient = num1 / num2
+    except ZeroDivisionError:
+        print("Can not divide by 0.", file = f)
+        sys.exit()
+
+    print("Product: ", product, file = f)
+    print("Quotient: ", quotient, file = f)
+
+    f.close()
+    os.chmod("CheckForSpeedResults.txt", stat.S_IREAD)
 
 if __name__ == "__main__":
     main()

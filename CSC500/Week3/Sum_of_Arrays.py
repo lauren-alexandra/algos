0649365b87c598f7a49d58e8e9ce69e1106354d7
Lauren Alexandra <lauren@laurenalexandra.dev>
2021-02-23 16:58:52 -0800

    helper funcs

--- CSC500/Week3/Sum_of_Arrays.py
+++ CSC500/Week3/Sum_of_Arrays.py
@@ -14,7 +14,7 @@ import sys
 import array
 
 def main():
-    f = open('SumOfArrays.txt', 'w+')
+    f = open('SumofArrays.txt', 'w+')
 
     first_arr = array.array('f', [])
     second_arr = array.array('f', [])
@@ -28,15 +28,15 @@ def main():
     third_input = input('Enter the numbers of the third array: ')
     check_input(third_input, third_arr, arr_list)
 
-    indices = input('Enter the indices of the three elements starting with the first array: ')
-    elements = check_indices(indices, arr_list)
+    indices_input = input('Enter the indices of the three elements starting with the first array: ')
+    indices = check_indices(indices_input, arr_list)
 
     try:
         target = float(input('Enter the target sum: '))
-        sum = find_sum(elements, arr_list)
+        sum = find_sum(indices, arr_list)
 
-        print("Target: ", target , file = f)
-        print("Sum: ", sum, file = f)
+        print("Target = ", target , file = f)
+        print("Sum = ", sum, file = f)
         print("Target and sum are equal." if target == sum else "Not equal.", file = f)
 
     except ValueError:
@@ -45,11 +45,12 @@ def main():
 
     f.close()
 
-def find_sum(elements_, list_):
+
+def find_sum(indices_, list_):
     sum = 0
 
-    for (elem, arr) in zip(elements_, list_):
-        sum += arr[elem]
+    for (index, arr) in zip(indices_, list_):
+        sum += arr[index]
 
     return sum
 
@@ -65,19 +66,19 @@ def check_input(input, arr, list_):
         sys.exit()  
 
 
-def check_indices(indices_, list_):
+def check_indices(input_, list_):
     try:
-        elements = list(map(int, indices_.split(" ")))
-        if len(elements) != 3:
-            print('Not at least 3 indices provided.', file = f)
+        indices_ = list(map(int, input_.split(" ")))
+        if len(indices_) != 3:
+            print('Three indices not provided.', file = f)
             sys.exit()
 
-        for (elem, arr) in zip(elements, list_): 
-            if elem > len(arr) - 1:
+        for (index, arr) in zip(indices_, list_): 
+            if index > len(arr) - 1:
                 print('Indices must be in bounds.', file = f)
                 sys.exit()
 
-        return elements
+        return indices_
 
     except ValueError:
         print('Not numbers.', file = f)

8d18f2edc1fbce2a264f718a324f831fb6309a0b
Lauren Alexandra <lauren@laurenalexandra.dev>
2021-03-02 10:30:28 -0800

    abstract vs struct

---
+++ interview-help/leet-code-explore/stacks.py
@@ -0,0 +1,29 @@
+# https://bradfieldcs.com/algos/stacks/introduction/
+
+"""
+Stacks
+
+Linear data structure (stacks, queues, deques, and lists):  Once an item is added, it stays in the same position relative to its neighbors. 
+
+Linear structures can be thought of as having two ends. What distinguishes one linear structure from another is where additions and removals may occur.
+
+A stack is an ordered collection of items where the addition of new items and the removal of existing items always takes place at the same end. 
+
+This end is commonly referred to as the “top”, and the opposite end is known as the “base”.
+
+Top Plate (the end of the stack)  --> will be removed first. The insertion order is the reverse of the removal order.
+
+Plate 
+
+Plate
+
+Base Plate (beginning of the stack)
+
+
+An abstract data type means that we’re only concerned with what the data represents and not with how it’ll be constructed.
+
+A data structure is an implementation of an abstract data type.
+
+The separation of these two perspectives allows us to define complex data models for our problems without giving any 
+details about how the model will actually be built.
+"""

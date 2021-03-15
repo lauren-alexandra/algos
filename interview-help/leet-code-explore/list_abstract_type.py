b7dd423c7b911b0a8174f2bbf4c8c8c68d62825e
Lauren Alexandra <lauren@laurenalexandra.dev>
2021-03-15 11:25:26 -0700

    unordered, ordered lst

---
+++ interview-help/leet-code-explore/list_abstract_type.py
@@ -0,0 +1,43 @@
+"""
+https://bradfieldcs.com/algos/lists/introduction/
+
+Lists
+"""
+
+"""
+Unordered List Abstract Data Type
+
+List() creates a new list.
+
+add(item) adds item and returns nothing.
+
+remove(item) removes item and returns nothing.
+
+search(item) searches for item and returns boolean value.
+
+is_empty() returns a boolean value.
+
+size() returns an integer
+
+append(item) appends to the END of the list.
+
+index(item) returns position of list item.
+
+insert(position, item) adds a new item to the list at the position.
+
+pop() removes and returns the last item in the list.
+
+pop(position) removes and returns the item at position.
+"""
+
+"""
+Ordered List
+
+items are maintained in meaningful order
+
+e.g. add 2, 1, 3 to an ordered list.
+
+get 1 -> 2 -> 3
+or 
+3 -> 2 -> 1
+"""

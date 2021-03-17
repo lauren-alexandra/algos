d4ec542d293ea6e5c86ce6635259cd4367fa148a
Lauren Alexandra <lauren@laurenalexandra.dev>
2021-03-17 15:48:36 -0700

    init milestone

---
+++ CSC500/Week6/Portfolio_Milestone/Soccer_Team_Roster.py
@@ -0,0 +1,23 @@
+"""
+Title: Soccer Team Roster
+Description: 
+
+Term: Winter D
+Course: CSC 500
+Instructor: Amr Elchouemi
+
+Author: Lauren Alexandra
+Email: lauren.alexandra@csuglobal.edu
+"""
+
+"""
+NOTE: YOU MAY NEED TO BRING IN STEPS 1-2 FROM WEEK3 PORTFOLIO
+
+Step 3 (of portfolio): Prompt the user to input five pairs of numbers: A player's jersey number (0 - 99) and the player's rating (1 - 9). Store the jersey numbers and the ratings in a dictionary. Output the dictionary's elements with the jersey numbers in ascending order (i.e., output the roster from smallest to largest jersey number). Hint: Dictionary keys can be stored in a sorted list. (3 pts)
+
+(see example in Assignment Instructions)
+
+Step 4 (of portfolio): Step 4: Implement a menu of options for a user to modify the roster. Each option is represented by a single character. The program initially outputs the menu after a user chooses an option. The program ends when the user chooses the option to Quit. For this step, the other options do nothing.
+
+(see example in Assignment Instructions)
+"""

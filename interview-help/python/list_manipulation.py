e7e85f01989eaf2a581f230f31a6a403d6396b9e
Lauren Alexandra <lauren@laurenalexandra.dev>
2021-03-27 17:17:13 -0700

    lsts

--- interview-help/python/list_manipulation.py
+++ interview-help/python/list_manipulation.py
@@ -46,3 +46,39 @@ print(elems)
 
 # Insertion
 
+elems[1] = 10 # change value at index
+elems[1:2] = [20, 30, 40] # replace values at indices
+elems[1:1] = [0.2, 0.3, 0.5] # Insert 3 values between index 0 and index 1
+
+
+# Flatten lists
+
+list_of_lists = [[1], [2, 3], [4, 5, 6]]
+sum(list_of_lists, [])
+# [1, 2, 3, 4, 5, 6]
+
+# flatten with list comprehension
+regular_list = [[1, 2, 3, 4], [5, 6, 7], [8, 9]]
+flat_list = [item for sublist in regular_list for item in sublist]
+print('Original list', regular_list)
+print('Transformed list', flat_list)
+# Original list [[1, 2, 3, 4], [5, 6, 7], [8, 9]]
+# Transformed list [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+"""
+Flatten List of Lists Using itertools (chain())
+
+This approach is ideal for transforming a 2-D list into a single flat list as 
+it treats consecutive sequences as a single sequence by iterating through the iterable 
+passed as the argument in a sequential manner.
+"""
+import itertools
+
+regular_list = [[1, 2, 3, 4], [5, 6, 7], [8, 9, 10]]
+flat_list = list(itertools.chain(*regular_list))
+
+print('Original list', regular_list)
+print('Transformed list', flat_list)
+# Original list [[1, 2, 3, 4], [5, 6, 7], [8, 9, 10]]
+# Transformed list [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]

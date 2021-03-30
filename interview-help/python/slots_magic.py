c6b1823dd6a3501942519c18d53f30a7c411af61
Lauren Alexandra <lauren@laurenalexandra.dev>
2021-03-30 11:09:47 -0700

    slots

---
+++ interview-help/python/slots_magic.py
@@ -0,0 +1,18 @@
+"""
+By default Python uses a dict to store an object’s instance attributes.
+
+The dict wastes a lot of RAM. Python can’t just allocate a static 
+amount of memory at object creation to store all the attributes. 
+Therefore it sucks a lot of RAM if you create a lot of objects 
+(I am talking in thousands and millions).
+
+__slots__ tells Python not to use a dict, and only allocate space for 
+a fixed set of attributes. 
+"""
+
+class MyClass(object):
+    __slots__ = ['name', 'identifier']
+    def __init__(self, name, identifier):
+        self.name = name
+        self.identifier = identifier
+        self.set_up()

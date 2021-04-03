84966eff10811d54aa85f9af3638c4fe3e7019c8
Lauren Alexandra <lauren@laurenalexandra.dev>
2021-04-02 21:44:16 -0700

    one liners

---
+++ interview-help/python/list_flattening.py
@@ -0,0 +1,8 @@
+# You can quickly and easily flatten a list using itertools.chain.from_iterable from the itertools package. 
+a_list = [[1, 2], [3, 4], [5, 6]]
+print(list(itertools.chain.from_iterable(a_list)))
+# Output: [1, 2, 3, 4, 5, 6]
+
+# or
+print(list(itertools.chain(*a_list)))
+# Output: [1, 2, 3, 4, 5, 6]
---
+++ interview-help/python/profiling_a_script.py
@@ -0,0 +1,2 @@
+# This can be extremely helpful in pinpointing the bottlenecks in your scripts:
+python -m cProfile my_script.py

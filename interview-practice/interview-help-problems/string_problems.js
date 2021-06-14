daf634e7a9a0e3232a4863d30bed8ae6beba2d0f
Lauren Alexandra <lauren@laurenalexandra.dev>
2021-06-14 10:53:54 -0700

    valid pal

--- interview-practice/interview-help-problems/string_problems.js
+++ interview-practice/interview-help-problems/string_problems.js
@@ -18,61 +18,67 @@ Output: false
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function(s) {
-    // set two pointers, one at each end of the string
-    // traverse inwards
-    // if space, skip comparison
-    // otherwise compare eq. if not eq, return false
+ var isPalindrome = function(s) {
+  // set two pointers, one at each end of the string
+  // traverse inwards
+  // if space, skip comparison
+  // otherwise compare eq. if not eq, return false
+   
+  var firstP = 0;
+  var secondP = s.length - 1;
+   
+  var alphaReg = /[a-z]/i;  
+  var numReg = /^\d+$/; 
+  var first, second;
+   
+  while (firstP < secondP) {
+   // check first pointer 0-9 or a-z 
+   if(s[firstP].match(alphaReg) || s[firstP].match(numReg)) {
+      // convert to lowercase if str
+      if (s[firstP].match(alphaReg)) {
+         first = s[firstP].toLowerCase(); 
+      } else {
+          first = s[firstP].match(numReg);  
+      }     
      
-    var firstP = 0;
-    var secondP = s.length - 1;
-     
-    var alphaReg = /[a-z]/i;  
-    var numReg = /^\d+$/; 
-    var first, second;
-     
-    while (firstP < secondP) {
-     // check first pointer 0-9 or a-z 
-     if(s[firstP].match(alphaReg) || s[firstP].match(numReg)) {
+     // check second pointer 0-9 or a-z 
+      if(s[secondP].match(alphaReg) || s[secondP].match(numReg)) {
         // convert to lowercase if str
-        if (s[firstP].match(alphaReg)) {
-           first = s[firstP].toLowerCase(); 
+        if (s[secondP].match(alphaReg)) {
+          second = s[secondP].toLowerCase();
         } else {
-            first = s[firstP].match(numReg);  
+          second = s[secondP].match(numReg); 
         }     
-       
-       // check second pointer 0-9 or a-z 
-        if(s[secondP].match(alphaReg) || s[secondP].match(numReg)) {
-          // convert to lowercase if str
-          if (s[secondP].match(alphaReg)) {
-            second = s[secondP].toLowerCase();
-          } else {
-            second = s[secondP].match(numReg); 
-          }     
-       
-          // compare eq 
-          if (first != second) {
-           return false;  
-          } 
-          // if match 
-          else {
-           firstP++;
-           secondP--;  
-          }
-          
-        }
+     
+        // compare eq 
+        if (first != second) {
+         console.log("Not Valid");
+         return false;
+        } 
+        // if match 
         else {
-          secondP--; 
+         firstP++;   // traverse inwards from front
+         secondP--;  // traverse inwards from back
         }
-     } 
-     // if char not alphanumeric  
-     else {
-      firstP++; 
-     }
-    }
-       
-    return true;    
+        
+      }
+      else {
+        secondP--; 
+      }
+   } 
+   // if char not alphanumeric aka it's likely a space so move forward
+   else {
+    firstP++; 
+   }
+  }
+     
+  console.log("Valid Palindrome");    
+  return true;
 };
+
+// test
+isPalindrome("race a car"); // will log Not Valid
+isPalindrome("A man, a plan, a canal: Panama"); // will log Valid Palindrome
 
 /*
 #151 Reverse Words in a String

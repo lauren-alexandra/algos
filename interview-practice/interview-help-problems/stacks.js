ce22da8ec6203094d54aa75e7c7b2d1b8ff2a1e5
Lauren Alexandra <lauren@laurenalexandra.dev>
2021-07-19 11:58:42 -0700

    stack bb

--- interview-practice/interview-help-problems/stacks.js
+++ interview-practice/interview-help-problems/stacks.js
@@ -1,3 +1,10 @@
+/*
+Top of stack is at the end of the list
+
+LIFO - last in first out
+think of a stack of plates 
+*/
+
 // #232 implement queue using stacks // success
 
 var MyQueue = function() {
@@ -169,5 +176,69 @@ Output: 6
     
     return stack.pop(); // aka return last score which is current
 };
+
+
+/*
+682. Baseball Game
+
+You are keeping score for a baseball game with strange rules. The game consists of several rounds, where the scores of past rounds may affect future rounds' scores.
+
+At the beginning of the game, you start with an empty record. You are given a list of strings ops, where ops[i] is the ith operation you must apply to the record and is one of the following:
+
+An integer x - Record a new score of x.
+"+" - Record a new score that is the sum of the previous two scores. It is guaranteed there will always be two previous scores.
+"D" - Record a new score that is double the previous score. It is guaranteed there will always be a previous score.
+"C" - Invalidate the previous score, removing it from the record. It is guaranteed there will always be a previous score.
+Return the sum of all the scores on the record.
+
+ 
+
+Example 1:
+
+Input: ops = ["5","2","C","D","+"]
+Output: 30
+Explanation:
+"5" - Add 5 to the record, record is now [5].
+"2" - Add 2 to the record, record is now [5, 2].
+"C" - Invalidate and remove the previous score, record is now [5].
+"D" - Add 2 * 5 = 10 to the record, record is now [5, 10].
+"+" - Add 5 + 10 = 15 to the record, record is now [5, 10, 15].
+The total sum is 5 + 10 + 15 = 30.
+*/
+/**
+ * @param {string[]} ops
+ * @return {number}
+ */
+ var calPoints = function(ops) {
+    let record = []; 
+    
+    for (let op of ops) {
+        op = parseInt(op) ? parseInt(op) : op;
+
+        if (typeof op === 'number') {
+            record.push(op); 
+        }
+        else if (op === '+') {
+            let sumScore = record[record.length - 1] + record[record.length - 2];
+            record.push(sumScore);
+        }
+        else if (op === 'D') {
+            let doublePrev = record[record.length - 1] * 2;
+            record.push(doublePrev);
+        }
+        else {
+            record.pop(); 
+        }
+    }
+    
+    const finalSum = record.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+    }, 0)
+
+    
+    return finalSum; 
+};
+
+
 
 

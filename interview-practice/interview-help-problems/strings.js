6b5a4433c9c536a80bfdf5ab50541444142b0f56
Lauren Alexandra <lauren@laurenalexandra.dev>
2021-07-20 22:57:36 -0700

    patterns

--- interview-practice/interview-help-problems/strings.js
+++ interview-practice/interview-help-problems/strings.js
@@ -378,3 +378,62 @@ Output: "doG gniD"
   
   return reversed.join(' ');
 };
+
+/*
+412. Fizz Buzz
+
+Given an integer n, return a string array answer (1-indexed) where:
+
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i if non of the above conditions are true.
+ 
+
+Example 1:
+
+Input: n = 3
+Output: ["1","2","Fizz"]
+Example 2:
+
+Input: n = 5
+Output: ["1","2","Fizz","4","Buzz"]
+
+Complexity Analysis
+
+Time Complexity: O(N)O(N)
+Space Complexity: O(1)O(1)
+*/
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+ var fizzBuzz = function(n) {
+  let answer = [];
+  
+  for (let i = 1; i <= n; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+          answer.push('FizzBuzz');
+      }
+
+      else if (i % 3 === 0) {
+          answer.push('Fizz');
+      }
+
+      else if (i % 5 === 0) {
+          answer.push('Buzz');
+      }
+
+      else {
+          answer.push(i.toString())
+      }   
+  }
+  
+  return answer;
+};
+
+
+
+
+
+

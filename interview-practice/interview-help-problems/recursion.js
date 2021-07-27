3a2243c962e39c7fa9d89d96d8d2e7a8f88c59fd
Lauren Alexandra <lauren@laurenalexandra.dev>
2021-07-27 12:37:47 -0700

    reverse list

--- interview-practice/interview-help-problems/recursion.js
+++ interview-practice/interview-help-problems/recursion.js
@@ -68,4 +68,90 @@ To solve the problem, call helper function passing the head and tail indexes as 
 };
 
 
+/*
+206. Reverse Linked List
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Example:
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+*/
+
+// Approach #1 (Iterative) 
+/*
+While you are traversing the list, change the current node's next pointer to 
+point to its previous element. Since a node does not have reference to its 
+previous node, you must store its previous element beforehand. You also need 
+another pointer to store the next node before changing the reference. Do not 
+forget to return the new head reference at the end!
+*/
+/*
+Complexity analysis
+
+Time complexity : O(n). Assume that nn is the list's length, the time complexity is O(n)O(n).
+
+Space complexity : O(1).
+*/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var reverseList = function(head) {
+    
+    let prev = null; 
+    let curr = head;
+    
+    while (curr !== null) {
+        let currentNext = curr.next; 
+        curr.next = prev;
+        prev = curr;
+        curr = currentNext;
+    }
+    
+    return prev; // modified linked list
+};
+
+// Approach #2 (Recursive)
+/*
+The recursive version is slightly trickier and the key is to work backwards. 
+Assume that the rest of the list had already been reversed, now how do I reverse the front part?
+*/
+/*
+Complexity analysis
+
+Time complexity : O(n). Assume that n is the list's length, the time complexity is O(n)O(n).
+
+Space complexity : O(n). The extra space comes from implicit stack space due to recursion. The recursion could go up to nn levels deep.
+*/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var reverseList = function(head) {
+    if (head === null || head.next === null) {
+        return head;
+    }
+    
+    let p = reverseList(head.next);
+    head.next.next = head; 
+    head.next = null; // this puts the node at the end
+    
+    return p; 
+};
+
 

215b305ceece2d073e7e2bf991035354adbb0316
Lauren Alexandra <lauren@laurenalexandra.dev>
2021-08-03 14:22:11 -0700

    3d projection

--- interview-practice/interview-help-problems/arrays.js
+++ interview-practice/interview-help-problems/arrays.js
@@ -485,3 +485,71 @@ Since all customers got correct change, we output true.
     return true;
 };
 
+/*
+883. Projection Area of 3D Shapes
+
+You are given an n x n grid where we place some 1 x 1 x 1 cubes that are axis-aligned with the x, y, and z axes.
+
+Each value v = grid[i][j] represents a tower of v cubes placed on top of the cell (i, j).
+
+We view the projection of these cubes onto the xy, yz, and zx planes.
+
+A projection is like a shadow, that maps our 3-dimensional figure to a 2-dimensional plane. We are viewing the "shadow" when looking at the cubes from the top, the front, and the side.
+
+Return the total area of all three projections.
+
+Example 1:
+Input: grid = [[1,2],[3,4]]
+Output: 17
+Explanation: Here are the three projections ("shadows") of the shape made with each axis-aligned plane.
+*/
+
+/*
+Solution
+
+Approach 1: Mathematical
+Intuition and Algorithm
+
+From the top, the shadow made by the shape will be 1 square for each non-zero value.
+
+From the side, the shadow made by the shape will be the largest value for each row in the grid.
+
+From the front, the shadow made by the shape will be the largest value for each column in the grid.
+
+Example
+
+With the example [[1,2],[3,4]]:
+
+The shadow from the top will be 4, since there are four non-zero values in the grid;
+
+The shadow from the side will be 2 + 4, since the maximum value of the first row is 2, and the maximum value of the second row is 4;
+
+The shadow from the front will be 3 + 4, since the maximum value of the first column is 3, and the maximum value of the second column is 4.
+*/
+
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+ var projectionArea = function(grid) {
+    let answer = 0;
+    
+    for (let i = 0; i < grid.length; i++) {
+        let bestRow = 0; 
+        let bestCol = 0;
+        
+        for (let j = 0; j < grid.length; j++) {
+            if (grid[i][j] > 0) {
+                answer += 1; // add to top shadow
+            }
+            bestRow = Math.max(bestRow, grid[i][j]);
+            bestCol = Math.max(bestCol, grid[j][i]);
+        }
+        answer += bestRow + bestCol; 
+    }
+    
+    return answer;
+};
+
+
+

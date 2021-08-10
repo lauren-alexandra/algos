6c925272ba7091e5de35a36d9d1133af45cca5e1
Lauren Alexandra <lauren@laurenalexandra.dev>
2021-08-10 15:29:26 -0700

    arr single scan

--- interview-help-problems/arrays.js
+++ interview-help-problems/arrays.js
@@ -769,3 +769,49 @@ Output: false
     
     return result; 
 };
+
+/*
+605. Can Place Flowers
+
+You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule.
+
+ 
+
+Example 1:
+
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: true
+*/
+
+/*
+Solution:
+Approach #1 Single Scan [Accepted]
+The solution is very simple. We can find out the extra maximum number of flowers, count, that can be planted for the given flowerbed arrangement. To do so, we can traverse over all the elements of the flowerbed and find out those elements which are 0(implying an empty position). For every such element, we check if its both adjacent positions are also empty. If so, we can plant a flower at the current position without violating the no-adjacent-flowers-rule. For the first and last elements, we need not check the previous and the next adjacent positions respectively.
+
+If the count obtained is greater than or equal to nn, the required number of flowers to be planted, we can plant nn flowers in the empty spaces, otherwise not.
+*/
+
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+ var canPlaceFlowers = function(flowerbed, n) {
+    let i = 0;
+    let count = 0; 
+    
+    while (i < flowerbed.length) {
+        if (flowerbed[i] === 0 && (i === 0 || flowerbed[i - 1] === 0) && (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)) {
+            flowerbed[i] = 1;
+            count++;
+        }
+        i++
+    }
+    
+    return count >= n;
+};
+
+
+

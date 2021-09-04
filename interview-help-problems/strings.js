4d48465f864eee669eeddfc2b6993e1cb9c3ff01
Lauren Alexandra <lauren@laurenalexandra.dev>
2021-09-04 16:09:13 -0700

    sim sent

--- interview-help-problems/strings.js
+++ interview-help-problems/strings.js
@@ -645,3 +645,79 @@ Output: []
 };
 
 
+/*
+734. Sentence Similarity
+
+We can represent a sentence as an array of words, for example, the sentence "I am happy with leetcode" can be represented as arr = ["I","am",happy","with","leetcode"].
+
+Given two sentences sentence1 and sentence2 each represented as a string array and given an array of string pairs similarPairs where similarPairs[i] = [xi, yi] indicates that the two words xi and yi are similar.
+
+Return true if sentence1 and sentence2 are similar, or false if they are not similar.
+
+Two sentences are similar if:
+
+They have the same length (i.e., the same number of words)
+sentence1[i] and sentence2[i] are similar.
+Notice that a word is always similar to itself, also notice that the similarity relation is not transitive. For example, if the words a and b are similar, and the words b and c are similar, a and c are not necessarily similar.
+
+ 
+
+Example 1:
+
+Input: sentence1 = ["great","acting","skills"], sentence2 = ["fine","drama","talent"], similarPairs = [["great","fine"],["drama","acting"],["skills","talent"]]
+Output: true
+Explanation: The two sentences have the same length and each word i of sentence1 is also similar to the corresponding word in sentence2.
+Example 2:
+
+Input: sentence1 = ["great"], sentence2 = ["great"], similarPairs = []
+Output: true
+Explanation: A word is similar to itself.
+*/
+
+/**
+ * @param {string[]} sentence1
+ * @param {string[]} sentence2
+ * @param {string[][]} similarPairs
+ * @return {boolean}
+ */
+ var areSentencesSimilar = function(sentence1, sentence2, similarPairs) {
+  /*
+  you are given 2 sentences each an array of strings
+  
+  you are given an array which specifies which words are similar. 
+  
+  iterate through both sentences. create a pair array with each index and a reversed array
+  
+  check if similarPairs includes either pair array. 
+  if it doesn't return false. 
+  
+  first check
+  if the sentences are not the same length, return false
+  */
+  
+  if (sentence1.length !== sentence2.length) {
+      return false;
+  }
+  
+  if (sentence1.length === 1 && sentence2.length === 1) {
+      return sentence1[0] === sentence2[0];
+  }
+  
+  // you compare strings. can't compare arrays bc they are references to dif instances.
+  let pairsToCompare = similarPairs.map(pair => pair.join(''));
+  
+  for (let i = 0; i < sentence1.length; i++) {
+      let pair = [sentence1[i], sentence2[i]].join('');
+      let pairRev = [sentence2[i], sentence1[i]].join('');
+      
+      let pairCheck = pairsToCompare.includes(pair);
+      let pairRevCheck = pairsToCompare.includes(pairRev); 
+      
+      if (!pairCheck && !pairRevCheck) {
+          return false;
+      }
+  }
+      
+  return true;
+};
+

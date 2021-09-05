f07414b23ab52aec06febe8e527a80df2fd36879
Lauren Alexandra <lauren@laurenalexandra.dev>
2021-09-04 17:40:58 -0700

    valid abbr

--- interview-help-problems/strings.js
+++ interview-help-problems/strings.js
@@ -721,3 +721,84 @@ Explanation: A word is similar to itself.
   return true;
 };
 
+
+/*
+408. Valid Word Abbreviation
+
+A string can be abbreviated by replacing any number of non-adjacent substrings with their lengths. For example, a string such as "substitution" could be abbreviated as (but not limited to):
+
+"s10n" ("s ubstitutio n")
+"sub4u4" ("sub stit u tion")
+"12" ("substitution")
+"su3i1u2on" ("su bst i t u ti on")
+"substitution" (no substrings replaced)
+Note that "s55n" ("s ubsti tutio n") is not a valid abbreviation of "substitution" because the replaced substrings are adjacent.
+
+Given a string s and an abbreviation abbr, return whether the string matches with the given abbreviation.
+
+ 
+
+Example 1:
+
+Input: word = "internationalization", abbr = "i12iz4n"
+Output: true
+Example 2:
+
+Input: word = "apple", abbr = "a2e"
+Output: false
+*/
+/**
+ * @param {string} word
+ * @param {string} abbr
+ * @return {boolean}
+ */
+ var validWordAbbreviation = function(word, abbr) {
+  /*
+  given abbr
+  iterate through string by index
+  
+  when you reach an integer, change the increment
+  continue to iterate until next is non integer 
+  when you reach a letter set increment back to 1
+  */
+  
+  // edge case 
+  if (word.length === 1 && word === abbr || word.length === 1 && abbr === "1") {
+      return true;
+  }
+  
+  // split on an integer
+  let wordArr = abbr.split(/([0-9]+)/); 
+  let newWord = "";
+  
+  for (let item of wordArr) {
+      // check if value is an integer 
+      if (parseInt(item, 10)) {
+          // multiply a string 
+          let fill = "#".repeat(item); 
+          newWord += fill; 
+      }
+      else {
+          newWord += item;
+      }
+  }
+  
+  if (word.length !== newWord.length) {
+      return false; 
+  }
+  
+  for (let w = 0; w < newWord.length; w++) {
+      if (newWord[w] === "#") {
+          continue;
+      }
+      else {
+          if (newWord[w] !== word[w]) {
+              return false;
+          }
+      }
+  }
+  
+  return true;
+};
+
+

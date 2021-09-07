efa32a6cb70e80f9b6b7255e843ad10b003da853
Lauren Alexandra <lauren@laurenalexandra.dev>
2021-09-07 13:23:27 -0700

    sum less k

--- interview-help-problems/arrays.js
+++ interview-help-problems/arrays.js
@@ -1035,5 +1035,45 @@ sorted in increasing order as an array of indices.
     return results.sort((a, b) => a - b); 
 };
 
+/*
+1099. Two Sum Less Than K
+
+Given an array nums of integers and integer k, return the maximum sum such that there exists i < j with nums[i] + nums[j] = sum and sum < k. If no i, j exist satisfying this equation, return -1.
+
+ 
+
+Example 1:
+
+Input: nums = [34,23,1,24,75,33,54,8], k = 60
+Output: 58
+Explanation: We can use 34 and 24 to sum 58 which is less than 60.
+Example 2:
+
+Input: nums = [10,20,30], k = 15
+Output: -1
+Explanation: In this case it is not possible to get a pair sum less that 15.
+ 
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+ var twoSumLessThanK = function(nums, k) {
+    let answer = -1;
+    
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 1; j < nums.length; j++) {
+            let sum = nums[i] + nums[j];
+            
+            if (sum < k) {
+                answer = Math.max(answer, sum);
+            }
+        }
+    }
+    
+    return answer;
+};
 
 

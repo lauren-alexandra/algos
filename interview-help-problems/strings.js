83dfddf9247e2a820ce505515984a856e0251a9e
Lauren Alexandra <lauren@laurenalexandra.dev>
2021-09-09 17:31:08 -0700

    dest

--- interview-help-problems/strings.js
+++ interview-help-problems/strings.js
@@ -802,3 +802,69 @@ Output: false
 };
 
 
+/*
+1436. Destination City
+
+You are given the array paths, where paths[i] = [cityAi, cityBi] means there exists a direct path going from cityAi to cityBi. Return the destination city, that is, the city without any path outgoing to another city.
+
+It is guaranteed that the graph of paths forms a line without any loop, therefore, there will be exactly one destination city.
+
+ 
+
+Example 1:
+
+Input: paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
+Output: "Sao Paulo" 
+Explanation: Starting at "London" city you will reach "Sao Paulo" city which is the destination city. Your trip consist of: "London" -> "New York" -> "Lima" -> "Sao Paulo".
+Example 2:
+
+Input: paths = [["B","C"],["D","B"],["C","A"]]
+Output: "A"
+Explanation: All possible trips are: 
+"D" -> "B" -> "C" -> "A". 
+"B" -> "C" -> "A". 
+"C" -> "A". 
+"A". 
+Clearly the destination city is "A".
+Example 3:
+
+Input: paths = [["A","Z"]]
+Output: "Z"
+*/
+
+/**
+ * @param {string[][]} paths
+ * @return {string}
+ */
+ var destCity = function(paths) {
+  /*
+  a destination city has no outgoing path,
+  which means it must be on the right and never on the left
+  
+  so iterate through paths. 
+  add path[0] to incoming
+  add path[1] to outgoing
+  for each one in outgoing, check if its in incoming. if it's not, return it
+  */
+  
+  // edge cases
+  // paths length 1 return paths[i][1]
+  if (paths.length === 1) {
+      return paths[0][1];
+  }
+  
+  let incoming = paths.map(path => path[0]);
+  let outgoing = paths.map(path => path[1]);
+  
+  for (let path of outgoing) {
+      if (!incoming.includes(path)) {
+          return path;
+      }
+  }
+  
+  return -1;
+};
+
+
+
+

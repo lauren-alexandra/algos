68f8ff2cd956174fd4707bab9cd4628643845858
Lauren Alexandra <lauren@laurenalexandra.dev>
2021-11-20 20:13:26 -0800

    sm subtree traverse dfs

--- interview-help-problems/binary_tree_problems_DFS.py
+++ interview-help-problems/binary_tree_problems_DFS.py
@@ -499,3 +499,109 @@ class Solution:
 
       # return 0 as edge case if no root. 
       return 0 
+
+
+"""
+865. Smallest Subtree with all the Deepest Nodes
+
+Given the root of a binary tree, the depth of each node is the shortest distance to the root.
+
+Return the smallest subtree such that it contains all the deepest nodes in the original tree.
+
+A node is called the deepest if it has the largest depth possible among any node in the entire tree.
+
+The subtree of a node is a tree consisting of that node, plus the set of all descendants of that node.
+
+Example 1:
+Input: root = [3,5,1,6,2,0,8,null,null,7,4]
+Output: [2,7,4]
+Explanation: We return the node with value 2, colored in yellow in the diagram.
+The nodes coloured in blue are the deepest nodes of the tree.
+Notice that nodes 5, 3 and 2 contain the deepest nodes in the tree but node 2 is the smallest subtree among them, so we return it.
+
+Example 2:
+
+Input: root = [1]
+Output: [1]
+Explanation: The root is the deepest node in the tree.
+"""
+
+"""
+It is Traverse-and-accumulate template. 
+
+Traverse-and-accumulate template: we visit all the nodes with a traversal, and accumulate the wanted information in a nonlocal variable.
+
+If we need to accumulate some global information about the entire tree, we can use the traverse-and-accumulate template. 
+This template is like doing a for loop through all the nodes, which is often very convenient. It would be great to be able to simply do.
+
+O(n) because every node visited. 
+"""
+
+"""
+Discussion of the recursion in the tree aka how this line works in traversal() call:
+return max(cur_lvl, l_child_lvl, r_child_lvl)
+
+The most common pattern for solving binary tree problems is to compute things in the children and then aggregate them in the parent. 
+This is sometimes called the "bottom-up" approach. 
+It works really well with recursion: to solve a problem for the entire tree, we can solve it first for the subtrees of the root 
+(recursion takes care of that), and then use the results at the subtrees to solve the entire problem at the root.
+
+e.g. We compute something for each subtree recursively and pass it up to the root using the return value of the recursive function.
+"""
+
+# Template
+def solution(root):
+  res = ... #initial value
+ 
+  def visit(node):
+    if not node: return
+    res = ... #update res here
+    visit(node.left)
+    visit(node.right)
+  
+  visit(root)
+  return res
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def subtreeWithAllDeepest(self, root: TreeNode) -> TreeNode:
+        ans = root
+        max_depth = 0
+
+        def traversal(node, cur_lvl):
+          nonlocal ans, max_depth
+
+          if node == None:
+              return 0
+              
+          # search left if left node and increase the current level as you traverse down the tree
+          # we will get the return value from traversal as the max of current level, and the child levels
+          l_child_lvl = traversal(node.left, cur_lvl + 1)
+          
+          # search right if right node and increase the current level as you traverse down the tree
+          r_child_lvl = traversal(node.right, cur_lvl + 1)
+          
+          # continute to update the max depth as appropriate 
+          if cur_lvl > max_depth:
+              max_depth = cur_lvl
+              
+          if l_child_lvl == r_child_lvl == max_depth or cur_lvl == max_depth:
+              # You want to return the node containing the subtree 
+              # If the left child level and right child level are both the max depth (smallest subtree with greatest depth)
+              # Or if the current level is equal to the max depth. This would be the case if there was one level in the tree.
+              ans = node 
+
+          # to set the left and right child levels appropriately (see above), we need to return max depth of the 3
+          # to return the max from traversal()  
+          return max(cur_lvl, l_child_lvl, r_child_lvl)
+
+        traversal(root, 0)
+        
+        # return new subtree if exists 
+        return ans
+

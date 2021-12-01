4997d2ad38aadf4795725dd638dbe9bbe5c75aa4
Lauren Alexandra <lauren@laurenalexandra.dev>
2021-11-30 20:23:31 -0800

    min leaf dep

--- problems/binary_trees/binary_tree_BFS.py
+++ problems/binary_trees/binary_tree_BFS.py
@@ -32,3 +32,42 @@ def solution(root):
             if node.left: Q.append(node.left)
             if node.right: Q.append(node.right)
     return len(res) 
+
+
+"""
+Minimum leaf depth binary tree
+
+Given binary tree [3, 9, 20, null, null, 15, 7]
+return 2, the depth of the node with value 9.
+
+class TreeNode(object):
+    def __init__(self, x): 
+        self.val = x
+        self.left = None
+        self.right = None
+"""
+
+# return res length if no node left and no node right
+# else return res length at the end of traversal all nodes
+
+from collections import deque 
+
+def solution(root):
+    res = []
+    Q = deque([root])
+    while Q: 
+        res.append([])
+        currLevelSize = len(Q)
+        for i in range(currLevelSize):
+            node = Q.popleft()
+            res[-1].append(node.val)
+            # min leaf
+            if not node.left and not node.right:
+                return len(res)
+            if node.left: Q.append(node.left)
+            if node.right: Q.append(node.right)
+    return len(res) 
+
+
+
+
